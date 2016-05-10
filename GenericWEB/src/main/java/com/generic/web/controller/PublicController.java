731556954d213fa3cd7c64864daf53ef8b368c91
pepgonzalez <pepegonzalez.hdz@gmail.com>
2016-05-09 22:38:57 -0500

    security demo hardcoded test

--- GenericWEB/src/main/java/com/generic/web/controller/PublicController.java
+++ GenericWEB/src/main/java/com/generic/web/controller/PublicController.java
@@ -1,10 +1,19 @@
 package com.generic.web.controller;
 
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.generic.service.IGenericService;
 
@@ -21,5 +30,27 @@ public class PublicController {
 		LOG.debug("mostrando index");
 		genericService.test();
 		return "index";
+	}
+	
+	@RequestMapping("/auth/")
+	public String showAuth(){
+		LOG.debug("mostrando index logueado");
+		return "auth";
+	}
+	
+	@RequestMapping("/403")
+	public String showPermisionDenied(Principal user){
+		LOG.debug("mostrando permiso denegado");
+		genericService.test();
+		return "403";
+	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String doLogout (HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "redirect:/login?logout";
 	}
 }
